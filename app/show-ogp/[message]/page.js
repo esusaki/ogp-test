01f1f56bf2692edf1ad7f80f8cfd454647bfb247
esusaki <susaki2002@gmail.com>
2024-10-01 11:48:18 +0900

    hoge

--- app/show-ogp/[message]/page.js
+++ app/show-ogp/[message]/page.js
@@ -3,7 +3,18 @@ export default async function aiueo({params}){
     const {message} = params;
 
     return (
-            <div>hello, {decodeURI(message)}! </div>
+        <html>
+            <head>
+                <title>OGP sample</title>
+                <meta
+                    property="og:image"
+                    // content="https://og-examples.vercel.sh/api/static"
+                    content= {"https://ogp-test-omega.vercel.app/api/og/よいしょよいしょ！"}
+                />
+            </head>
+            <body>hello, {decodeURI(message)}! </body>
+        </html>
 
     )
 }
+
