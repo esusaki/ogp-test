b6676e6994305270ff04d1b626b178e6079dec76
esusaki <susaki2002@gmail.com>
2024-10-02 21:31:54 +0900

    hoge

--- app/show-ogp/[message]/page.js
+++ app/show-ogp/[message]/page.js
@@ -27,6 +27,8 @@ export default function Aiueo({params}){
                     // content="https://og-examples.vercel.sh/api/static"
                     content= {"https://ogp-test-omega.vercel.app/api/og/" + message}
                 />
+                <meta property = "og:image:width" content = "1200"/>
+                <meta property = "og:image:height" content = "630"/>
             </head>
             <body></body>
         </div>
