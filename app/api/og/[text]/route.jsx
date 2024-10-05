c6c3efed9aba8e11446cbb80e20bc9e167afc006
esusaki <susaki2002@gmail.com>
2024-10-05 21:40:01 +0900

    hoge-

--- app/api/og/[text]/route.jsx
+++ app/api/og/[text]/route.jsx
@@ -8,7 +8,7 @@ export async function GET(req, {params}) {
     (
       <div
         style={{
-          backgroundImage: "url(http://localhost:3000/matsuri_background.png)",
+          backgroundImage: "url(https://ogp-test-omega.vercel.app/matsuri_background.png)",
           color: 'black',
           background: 'white',
           width: '100%',
