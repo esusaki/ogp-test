d02c975218a92124cf8e7f8988336b279e05a0ee
esusaki <susaki2002@gmail.com>
2024-10-07 20:30:31 +0900

    hoge

--- app/api/og/[text]/route.jsx
+++ app/api/og/[text]/route.jsx
@@ -1,22 +1,28 @@
 import { ImageResponse } from 'next/og';
 // App router includes @vercel/og.
 // No need to install it.
+
+const font = fetch(
+  new URL("/assets/Kouzan.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
  
 export async function GET(req, {params}) {
   const {text} = params;
+  const fontData = await font;
   return new ImageResponse(
     (
       <div
         style={{
           backgroundImage: "url(https://ogp-test-omega.vercel.app/matsuri_background.png)",
-          color: 'black',
-          background: 'white',
+          color: 'white',
+          background: 'black',
           width: '100%',
           height: '100%',
           padding: '20px 30px',
           textAlign: 'center',
           justifyContent: 'center',
           alignItems: 'center',
+          fontFamily:'"Kouzan"',
           fontSize: 50
         }}
       >
@@ -26,6 +32,13 @@ export async function GET(req, {params}) {
     {
       width: 1200,
       height: 630,
+      fonts : [
+        {
+          name : "Kouzan",
+          data : fontData,
+          style : "normal"
+        }
+      ]
     },
   );
 }
